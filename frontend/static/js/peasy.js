d0804004d102e8a300276b2eb265aaf101fdc302
ajaywadhwa29 <ajaywadhwartk@gmail.com>
2017-06-20 22:14:33 +0530

    Ading Quiz Summary Page

--- frontend/static/js/peasy.js
+++ frontend/static/js/peasy.js
@@ -5,6 +5,7 @@ peMod.config(function($routeProvider) {
         .when('/login', { templateUrl: '/partials/login.html' })
         .when('/register', { templateUrl: '/partials/register.html' })
         .when('/company', { templateUrl: '/partials/company.html', controller: 'compayWiseCtrl' })
+		.when('/quizSummary', { templateUrl: '/partials/quizSummary.html', controller: 'quizSummaryCtrl' })
         .when('/topicwise', { templateUrl: '/partials/topicwise.html' })
         .when('/authortest', { templateUrl: '/partials/authorTest.html' })
         .when('/dashboard', { templateUrl: '/partials/dashboard.html' })
@@ -61,3 +62,275 @@ peMod.controller('TimepickerDemo', function($scope, $log) {
         $scope.mytime = null;
     };
 });
+
+/* AJAY START */
+peMod.controller('quizSummaryCtrl', function($scope) {
+	draw(120);
+    $scope.score=40;
+    $scope.Attempted=50;
+    $scope.correct=40;
+    $scope.inCorrect= 10;
+    $scope.NotAttempted= 50;
+    $scope.questions= [
+  {
+    "No": "1",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is : read only"
+  },
+  {
+    "No": "2",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+     
+  {
+    "No": "3",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+             
+  {
+    "No": "4",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+  {
+    "No": "5",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+  {
+    "No": "6",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+  {
+    "No": "7",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+  {
+    "No": "8",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },           
+  {
+    "No": "9",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+ {
+    "No": "10",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  }, 
+      {
+    "No": "11",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+  {
+    "No": "12",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+         {
+    "No": "13",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+         {
+    "No": "14",
+    "title": "work and energy",
+    "description": "this is my first question",
+    "options": [
+      "read only",
+      "skip now",
+      "do now",
+      "take rest"
+    ],
+    "Explanation": "Correct Answer is :    skip only"
+  },
+];
+        
+   
+    function redraw()
+    {
+        draw(document.getElementById("myList").value);
+    }
+    function draw(speed)
+    {
+          var  canvas = document.getElementById("myCanvas");
+          var  context = canvas.getContext("2d");
+          context.clearRect(0,0,canvas.width, canvas.height);
+          var centerX = canvas.width / 2;
+          var centerY = canvas.height / 2;
+          var radius = canvas.height / 2 - 20;
+
+          context.beginPath();
+          context.arc(centerX, centerY, radius, Math.PI*0.10, Math.PI*-1.1, true);
+
+          var gradience = context.createRadialGradient(centerX, centerY, radius-radius/2, centerX, centerY, radius-radius/8);
+           gradience.addColorStop(0, '#ff9000');
+           gradience.addColorStop(1, '#000000');
+
+           context.fillStyle = gradience;
+           context.fill();
+           context.closePath();
+           context.restore();
+
+        context.beginPath();
+        context.strokeStyle = '#ffff00';
+        context.translate(centerX,centerY);
+        var increment = 5;
+        context.font="15px Helvetica";
+        for (var i=-18; i<=18; i++)
+        {
+            angle = Math.PI/30*i;
+            sineAngle = Math.sin(angle);
+            cosAngle = -Math.cos(angle);
+
+            if (i % 5 == 0) {
+            context.lineWidth = 8;
+            iPointX = sineAngle *(radius -radius/4);
+            iPointY = cosAngle *(radius -radius/4);
+            oPointX = sineAngle *(radius -radius/7);
+            oPointY = cosAngle *(radius -radius/7);
+
+            wPointX = sineAngle *(radius -radius/2.5);
+            wPointY = cosAngle *(radius -radius/2.5);
+            context.fillText((i+18)*increment,wPointX-2,wPointY+4);
+            }
+            else
+            {
+            context.lineWidth = 2; 			
+            iPointX = sineAngle *(radius -radius/5.5);
+            iPointY = cosAngle *(radius -radius/5.5);
+            oPointX = sineAngle *(radius -radius/7);
+            oPointY = cosAngle *(radius -radius/7);
+            }
+            context.beginPath();
+            context.moveTo(iPointX,iPointY);
+            context.lineTo(oPointX,oPointY);
+            context.stroke();
+            context.closePath();
+        }
+        var numOfSegments = speed/increment;
+        numOfSegments = numOfSegments -18;
+        angle = Math.PI/30*numOfSegments;
+        sineAngle = Math.sin(angle);
+        cosAngle = -Math.cos(angle);
+        pointX = sineAngle *(3/4*radius);
+        pointY = cosAngle *(3/4*radius);
+
+        context.beginPath();
+        context.strokeStyle = '#000000';
+        context.arc(0, 0, 19, 0, 2*Math.PI, true);
+        context.fill();
+            context.closePath();
+
+        context.beginPath();    	
+        context.lineWidth=6;
+        context.moveTo(0,0);
+            context.lineTo(pointX,pointY);
+            context.stroke();
+            context.closePath();
+            context.restore();
+            context.translate(-centerX,-centerY);
+    }
+              
+   
+});
+
+/* AJAY END */
